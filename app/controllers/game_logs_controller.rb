bd249bccf84c11094e6ac59d7879c487b9d0587e
Alexandra D.D <alexandra.davisdonatelli@gartner.com>
2019-04-30 14:19:11 -0700

    Can now add ONE badge and rating

--- app/controllers/game_logs_controller.rb
+++ app/controllers/game_logs_controller.rb
@@ -6,22 +6,20 @@ class GameLogsController < ApplicationController
   def create
     @logs_timestamp = DateTime.new(params[:entry]["timestamp(1i)"].to_i,params[:entry]["timestamp(2i)"].to_i,params[:entry]["timestamp(3i)"].to_i,params[:entry]["timestamp(4i)"].to_i,params[:entry]["timestamp(5i)"].to_i)
 
-    @log_array = Array.new
+    @log_hash = Hash.new
 
     for i in 1..12
       if params[:entry]["badge_#{i}"].blank? == false
         add_badge = params[:entry]["badge_#{i}"].to_i
 
-        @log_array.push @logs_timestamp
-        @log_array.push params[:entry][:title]
-        @log_array.push params[:entry]["badge_#{i}"].to_i
-        @log_array.push params[:entry]["rating_#{i}"].to_i
+        @log_hash.merge!(timestamp: @logs_timestamp)
+        @log_hash.merge!(title: params[:entry][:title])
+        @log_hash.merge!(badge: params[:entry]["badge_#{i}"].to_i)
+        @log_hash.merge!(rating: params[:entry]["rating_#{i}"].to_i)
       end
     end
 
-    binding.pry
-
-    @logs = GameLog.new(@initial_params)
+    @logs = GameLog.new(@log_hash)
 
     if @logs.save
       redirect_to "/game_logs"
