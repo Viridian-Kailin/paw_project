67dceaac818b4903cbcdb564c58e2f2cb6e1c4ce
Alexandra D.D <alexandra.davisdonatelli@gartner.com>
2019-04-30 14:07:02 -0700

    Attempt #2 to submit game logs

--- app/controllers/game_logs_controller.rb
+++ app/controllers/game_logs_controller.rb
@@ -4,7 +4,24 @@ class GameLogsController < ApplicationController
   end
 
   def create
-    @logs = GameLog.new(initial_params)
+    @logs_timestamp = DateTime.new(params[:entry]["timestamp(1i)"].to_i,params[:entry]["timestamp(2i)"].to_i,params[:entry]["timestamp(3i)"].to_i,params[:entry]["timestamp(4i)"].to_i,params[:entry]["timestamp(5i)"].to_i)
+
+    @log_array = Array.new
+
+    for i in 1..12
+      if params[:entry]["badge_#{i}"].blank? == false
+        add_badge = params[:entry]["badge_#{i}"].to_i
+
+        @log_array.push @logs_timestamp
+        @log_array.push params[:entry][:title]
+        @log_array.push params[:entry]["badge_#{i}"].to_i
+        @log_array.push params[:entry]["rating_#{i}"].to_i
+      end
+    end
+
+    binding.pry
+
+    @logs = GameLog.new(@initial_params)
 
     if @logs.save
       redirect_to "/game_logs"
@@ -14,18 +31,6 @@ class GameLogsController < ApplicationController
   end
 
   def initial_params
-    @logs_timestamp = DateTime.new(params[:entry]["timestamp(1i)"].to_i,params[:entry]["timestamp(2i)"].to_i,params[:entry]["timestamp(3i)"].to_i,params[:entry]["timestamp(4i)"].to_i,params[:entry]["timestamp(5i)"].to_i)
-
-    @badgerating_array = Array.new
-
-    for i in 1..12
-      if params[:entry]["badge_#{i}"].blank? == false
-        add_badge = params[:entry]["badge_#{i}"].to_i
-
-        @badgerating_array.push params[:entry]["badge_#{i}"].to_i
-        @badgerating_array.push params[:entry]["rating_#{i}"].to_i
-      end
-    end
 
     # Place all key::values into a single, mass array
     params.require(:entry).permit(:title).merge(timestamp: @logs_timestamp)
