7605f322be4ec824e6a98a81fd1e6a1877b36fab
Alexandra D.D <alexandra.davisdonatelli@gartner.com>
2019-05-01 12:16:41 -0700

    Completed basic functionality for Game Logs.

--- app/controllers/game_logs_controller.rb
+++ app/controllers/game_logs_controller.rb
@@ -6,32 +6,21 @@ class GameLogsController < ApplicationController
   def create
     @logs_timestamp = DateTime.new(params[:entry]["timestamp(1i)"].to_i,params[:entry]["timestamp(2i)"].to_i,params[:entry]["timestamp(3i)"].to_i,params[:entry]["timestamp(4i)"].to_i,params[:entry]["timestamp(5i)"].to_i)
 
-
-    @log_hash = Hash.new
-
-    i = 0
-
-    for i in 1..12
-
-      if params[:entry]["badge_#{i}"].blank? == false
-        initial_hash = Hash.new
-
-        initial_hash.store :title, params[:entry][:title]
-        initial_hash.store :timestamp, @logs_timestamp
-        initial_hash.store :badge, params[:entry]["badge_#{i}"].to_i
-        initial_hash.store :rating, params[:entry]["rating_#{i}"].to_i
-
-        @log_hash.merge!(initial_hash)
-      end
+    12.times do |i|
       i = i + 1
+      if params[:entry]["badge_#{i}".to_sym].blank? == false
+        @log = GameLog.create(
+          title: params[:entry][:title],
+          timestamp: @logs_timestamp,
+          badge: params[:entry]["badge_#{i}".to_sym].to_i,
+          rating: params[:entry]["rating_#{i}".to_sym].to_i
+          )
+      end
     end
 
-    binding.pry
-
-    @logs = GameLog.new(@log_hash)
-
-    if @logs.save
+    if @log.save
       redirect_to "/game_logs"
+      flash[:notice] = "An entry for #{params[:entry][:title]} at #{@logs_timestamp} has been added."
     else
       render :show
     end
--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -1,17 +1,4 @@
 class GameLog < ApplicationRecord
   validates :title, presence: true
   serialize :entry, JSON
-
-  def entry_params
-    @badgerating_array = Array.new
-
-    for i in 1..12
-      if params[:entry]["badge_#{i}"].blank? == false
-        add_badge = params[:entry]["badge_#{i}"].to_i
-
-        @badgerating_array.push params[:entry]["badge_#{i}"].to_i => params[:entry]["rating_#{i}"].to_i
-      end
-    end
-  end
-
 end
